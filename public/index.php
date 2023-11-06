251235b44d7e68041e5229244ae57009b190670c
VoronczovaPolina <tirabyrius@gmail.com>
2023-11-06 22:16:31 +0500

    step 2

--- public/index.php
+++ public/index.php
@@ -4,14 +4,21 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use DI\Container;
 
-$app = AppFactory::create();
+$container = new Container();
+$container->set('renderer', function () {
+    // Параметром передается базовая директория, в которой будут храниться шаблоны
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+$app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
 $app->get(
     '/',
     function ($request, $response) {
-        return $response->write('Welcome to Slim!');
+        $params = ['write' => 'Welcome to Slim!'];
+        return $this->get('renderer')->render($response, 'index.phtml', $params);
     }
 );
 $app->run();
