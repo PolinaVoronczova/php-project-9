81e61ad87f9523544e27a291db64d548f3c02113
VoronczovaPolina <tirabyrius@gmail.com>
2023-11-14 07:24:58 +0500

    add DATABASE_URL connection

--- src/Connection.php
+++ src/Connection.php
@@ -17,25 +17,31 @@ final class Connection
      */
     public function connect()
     {
-        // чтение параметров в файле конфигурации ini
-        $params = parse_ini_file('database.ini');
+        if (getenv('DATABASE_URL')) {
+            $databaseUrl = parse_url(getenv('DATABASE_URL'));
+        }
+        if (isset($databaseUrl['host'])) {
+            $params['host'] = $databaseUrl['host'];
+            $params['port'] = isset($databaseUrl['port']) ? $databaseUrl['port'] : null;
+            $params['database'] = isset($databaseUrl['path']) ? ltrim($databaseUrl['path'], '/') : null;
+            $params['user'] = isset($databaseUrl['user']) ? $databaseUrl['user'] : null;
+            $params['passw'] = isset($databaseUrl['pass']) ? $databaseUrl['pass'] : null;
+        } else {
+            $params = parse_ini_file('database.ini');
+        }
         if ($params === false) {
             throw new \Exception("Error reading database configuration file");
         }
-
-        // подключение к базе данных postgresql
         $conStr = sprintf(
             "pgsql:host=%s;port=%d;dbname=%s;user=%s;password=%s",
             $params['host'],
             $params['port'],
             $params['database'],
             $params['user'],
-            $params['password']
+            $params['passw']
         );
-
         $pdo = new \PDO($conStr);
         $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-
         return $pdo;
     }
 
