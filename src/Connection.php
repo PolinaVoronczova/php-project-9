a781212a4e057c0f78ff655f3a7fcae6fdff95b7
VoronczovaPolina <tirabyrius@gmail.com>
2023-11-14 07:58:45 +0500

    fix error

--- src/Connection.php
+++ src/Connection.php
@@ -25,21 +25,14 @@ final class Connection
             $params['port'] = isset($databaseUrl['port']) ? $databaseUrl['port'] : null;
             $params['database'] = isset($databaseUrl['path']) ? ltrim($databaseUrl['path'], '/') : null;
             $params['user'] = isset($databaseUrl['user']) ? $databaseUrl['user'] : null;
-            $params['passw'] = isset($databaseUrl['pass']) ? $databaseUrl['pass'] : null;
+            $params['password'] = isset($databaseUrl['pass']) ? $databaseUrl['pass'] : null;
         } else {
             $params = parse_ini_file('database.ini');
         }
         if ($params === false) {
             throw new \Exception("Error reading database configuration file");
         }
-        $conStr = sprintf(
-            "pgsql:host=%s;port=%d;dbname=%s;user=%s;password=%s",
-            $params['host'],
-            $params['port'],
-            $params['database'],
-            $params['user'],
-            $params['passw']
-        );
+        $conStr = "pgsql:host={$params['host']};dbname={$params['database']};user={$params['user']};port={$params['port']};password={$params['password']}";
         $pdo = new \PDO($conStr);
         $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         return $pdo;
