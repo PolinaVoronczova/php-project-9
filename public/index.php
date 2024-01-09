c1b0e9ee0429c37f5356c7ff1f0afae9708463bd
VoronczovaPolina <tirabyrius@gmail.com>
2024-01-09 21:44:43 +0500

    fix error

--- public/index.php
+++ public/index.php
@@ -171,7 +171,6 @@ $app->post('/urls/{url_id}/checks', function ($request, $response, $args) use ($
         : mb_strimwidth($description, 0, 600, "..."),
         ':created_at' => $created_at,
     ];
-    var_dump($urlParam);
     $stmt->execute($urlParam);
     return $response->withRedirect($router->urlFor('showUrl', ['id' => $args['url_id']]), 302);
 })->setName('addChecks');
